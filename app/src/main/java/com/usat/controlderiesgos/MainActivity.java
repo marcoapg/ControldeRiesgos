79326e9e86148725d0f53c0284974f6024d5f6ec
marqueziusat <marcoapg1398@gmail.com>
2023-01-28 14:26:47 -0500

    Cambios adicionales

--- app/src/main/java/com/usat/controlderiesgos/MainActivity.java
+++ app/src/main/java/com/usat/controlderiesgos/MainActivity.java
@@ -1,15 +1,63 @@
 package com.usat.controlderiesgos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText correoLogin;
+    EditText contrasenaLogin;
+    ImageView btnLogin;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hola como estas
+
+        correoLogin = findViewById(R.id.Email);
+        contrasenaLogin = findViewById(R.id.Password);
+        btnLogin = findViewById(R.id.imageView3);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                login();
+            }
+        });
     }
+
+    private void login() {
+        String emailLogin = correoLogin.getText().toString();
+        String passwordLogin = contrasenaLogin.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(emailLogin, passwordLogin).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Los datos son incorrectos, intente nuevamente!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
 }
