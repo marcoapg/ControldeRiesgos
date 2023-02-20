dd374c8c198036bd3bfa48732bbdde83ef8bc591
alimiSM <71573842@usat.pe>
2023-02-20 14:32:01 -0500

    ok

--- app/src/main/java/com/usat/controlderiesgos/NavigationDrawer.java
+++ app/src/main/java/com/usat/controlderiesgos/NavigationDrawer.java
@@ -4,11 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
@@ -49,7 +47,7 @@ public class NavigationDrawer extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_amenazas, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_amenazas, R.id.nav_tiposactivo,R.id.nav_unidadesorganizacionles,R.id.nav_vulnerabilidades)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
